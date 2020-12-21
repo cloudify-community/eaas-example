498132651f32dcc007aa47cc720303112ac580b3
Isaac Shabtay <isaac-s@users.noreply.github.com>
2020-12-21 12:09:02 -0800

    Updates

--- infra/dev/vm_with_psql/scripts/configure-psql.sh
+++ infra/dev/vm_with_psql/scripts/configure-psql.sh
@@ -6,3 +6,6 @@ echo ${master_password} > ${temp_pw_file}
 export PGSETUP_INITDB_OPTIONS="-U ${master_username} --pwfile ${temp_pw_file}"
 sudo postgresql-setup initdb
 rm -f ${temp_pw_file}
+
+ctx instance runtime-properties master_username ${master_username}
+ctx instance runtime-properties master_password ${master_password}
