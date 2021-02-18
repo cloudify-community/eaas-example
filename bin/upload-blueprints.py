d995031894acba64fc06b7d0e32de9fcc8d1dfee
Jakub Cierlik <jakub.cierlik@amartus.com>
2021-02-18 15:44:38 +0100

    Changed postgresql server creation to ARM

--- bin/upload-blueprints.py
+++ bin/upload-blueprints.py
@@ -7,7 +7,8 @@ import threading
 
 def perform(**kwargs):
     def _thread_body(blueprint_id, blueprint_path):
-        print("Uploading blueprint '{}' from {}".format(blueprint_id, blueprint_path))
+        print("Uploading blueprint '{}' from {}".format(blueprint_id,
+                                                        blueprint_path))
         subprocess.check_call(
             ['cfy', 'blueprints', 'upload', blueprint_path, '-b', blueprint_id])
 
@@ -22,14 +23,34 @@ def perform(**kwargs):
     for env_type in ['dev', 'prod']:
         env_blueprints = os.listdir(os.path.join(root_dir, 'infra', env_type))
         for env_blueprint in env_blueprints:
-            for file in os.listdir(os.path.join(root_dir, 'infra', env_type, env_blueprint)):
+            for file in os.listdir(
+                os.path.join(root_dir, 'infra', env_type, env_blueprint)
+            ):
                 if file.endswith("blueprint.yaml"):
                     if file.startswith("aws"):
-                        blueprints.append(("aws_{}".format(env_blueprint), os.path.join(root_dir, 'infra', env_type, env_blueprint, 'aws-blueprint.yaml')))
+                        blueprints.append(("aws_{}".format(env_blueprint),
+                                           os.path.join(root_dir,
+                                                        'infra',
+                                                        env_type,
+                                                        env_blueprint,
+                                                        'aws-blueprint.yaml')))
                     elif file.startswith("azure"):
-                        blueprints.append(("azure_{}".format(env_blueprint), os.path.join(root_dir, 'infra', env_type, env_blueprint, 'azure-blueprint.yaml')))
+                        blueprints.append(("azure_{}".format(env_blueprint),
+                                           os.path.join(root_dir,
+                                                        'infra',
+                                                        env_type,
+                                                        env_blueprint,
+                                                        'azure-blueprint.yaml'))
+                        )
                     else:
-                        blueprints.append((env_blueprint, os.path.join(root_dir, 'infra', env_type, env_blueprint, 'blueprint.yaml')))
+                        blueprints.append((
+                            env_blueprint,
+                            os.path.join(root_dir,
+                                         'infra',
+                                         env_type,
+                                         env_blueprint,
+                                         'blueprint.yaml'))
+                        )
 
     threads = []
     for blueprint_id, blueprint_path in blueprints:
