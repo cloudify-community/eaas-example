e5e23310a8e32dcbcbb20e7919be1122f7288501
Krzysztof Frukacz <krzysztof.frukacz@cloudify.co>
2021-09-14 14:12:36 +0200

    Fixed encoding

--- infra/dev/minikube/scripts/load-config.py
+++ infra/dev/minikube/scripts/load-config.py
@@ -10,20 +10,20 @@ kubeconfig_raw = os.popen('sudo cat ' + kubeconfig_path)
 kube_config_dict = yaml.safe_load(kubeconfig_raw.read())
 
 ca_path = '/home/docker/.minikube/ca.crt'
-ca_raw = os.popen('sudo cat ' + ca_path)
+ca_raw = os.popen('sudo cat ' + ca_path + ' | base64')
 crt_path = '/home/docker/.minikube/profiles/minikube/client.crt'
-crt_raw = os.popen('sudo cat ' + crt_path)
+crt_raw = os.popen('sudo cat ' + crt_path + ' | base64')
 key_path = '/home/docker/.minikube/profiles/minikube/client.key'
-key_raw = os.popen('sudo cat ' + key_path)
+key_raw = os.popen('sudo cat ' + key_path + ' | base64')
 
 kube_config_dict['clusters'][0]['cluster']['certificate-authority-data'] = \
-    ca_raw.read()
+    ''.join([line.rstrip('\n') for line in ca_raw.readlines()])
 kube_config_dict['clusters'][0]['cluster']['server'] = \
     'https://' + os.getenv('HOST_VM_IP')  + ':8443'
 kube_config_dict['users'][0]['user']['client-certificate-data'] = \
-    crt_raw.read()
+    ''.join([line.rstrip('\n') for line in crt_raw.readlines()])
 kube_config_dict['users'][0]['user']['client-key-data'] = \
-    key_raw.read()
+    ''.join([line.rstrip('\n') for line in key_raw.readlines()])
 del(kube_config_dict['clusters'][0]['cluster']['certificate-authority'])
 del(kube_config_dict['users'][0]['user']['client-certificate'])
 del(kube_config_dict['users'][0]['user']['client-key'])
