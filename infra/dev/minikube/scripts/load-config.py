8191b93d6a0e6e7f0d1778644b888cb549d2bca3
Krzysztof Frukacz <krzysztof.frukacz@cloudify.co>
2021-09-14 10:11:50 +0200

    Fix address fetch

--- infra/dev/minikube/scripts/load-config.py
+++ infra/dev/minikube/scripts/load-config.py
@@ -8,7 +8,6 @@ import yaml
 kubeconfig_path = '/home/docker/.kube/config'
 kubeconfig_raw = os.popen('sudo cat ' + kubeconfig_path)
 kube_config_dict = yaml.safe_load(kubeconfig_raw.read())
-# ctx.instance.runtime_properties['config'] = kube_config_dict
 
 ca_path = '/home/docker/.minikube/ca.crt'
 ca_raw = os.popen('sudo cat ' + ca_path)
@@ -17,13 +16,16 @@ crt_raw = os.popen('sudo cat ' + crt_path)
 key_path = '/home/docker/.minikube/profiles/minikube/client.key'
 key_raw = os.popen('sudo cat ' + key_path)
 
-kube_config_dict['clusters'][0]['cluster']['certificate-authority'] = \
+kube_config_dict['clusters'][0]['cluster']['certificate-authority-data'] = \
     ca_raw.read()
 kube_config_dict['clusters'][0]['cluster']['server'] = \
-    'https://' + str(HOST_VM_IP) + ':8443'
-kube_config_dict['users'][0]['user']['client-certificate'] = \
+    'https://' + os.getenv('HOST_VM_IP')  + ':8443'
+kube_config_dict['users'][0]['user']['client-certificate-data'] = \
     crt_raw.read()
-kube_config_dict['users'][0]['user']['client-key'] = \
+kube_config_dict['users'][0]['user']['client-key-data'] = \
     key_raw.read()
+del(kube_config_dict['clusters'][0]['cluster']['certificate-authority'])
+del(kube_config_dict['users'][0]['user']['client-certificate'])
+del(kube_config_dict['users'][0]['user']['client-key'])
 
 ctx.instance.runtime_properties['config'] = kube_config_dict
