5b3f8a3552c2c3560a2ce90e7271e11ae7281daf
Krzysztof Frukacz <krzysztof.frukacz@cloudify.co>
2021-09-15 15:03:43 +0200

    Removed use of secrets, add ttl

---
+++ environments/ttl/enable.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import datetime
+from cloudify import manager
+from cloudify import ctx
+from cloudify.state import ctx_parameters as inputs
+
+#SCHEDULE_FORMAT = '%Y%m%d%H%M%z'
+SCHEDULE_FORMAT = '%Y-%m-%d %H:%M'
+
+ttl = inputs['ttl']
+
+ctx.logger.debug("TTL is {}".format(ttl))
+
+if ttl > 0:
+    schedule_time = (datetime.datetime.now() + \
+                     datetime.timedelta(minutes=ttl)).strftime(
+                     SCHEDULE_FORMAT)
+
+
+    ctx.logger.info("This deployment will be uninstalled at {}".format(schedule_time))
+    execution_id = manager.get_rest_client().executions.start(
+        deployment_id = ctx.deployment.id,
+        workflow_id = "uninstall",
+        parameters = {"ignore_failure": True},
+        schedule = schedule_time).id
+
+    ctx.instance.runtime_properties['execution_id'] = execution_id
