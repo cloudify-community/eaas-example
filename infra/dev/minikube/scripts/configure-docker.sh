260cb9a78102fac97a0fcd5eea2a4c34c68eb772
jrzeszutek <jaroslaw@cloudify.co>
2021-09-30 15:47:29 +0200

    added load balancer service, which works on all types of envs

--- infra/dev/minikube/scripts/configure-docker.sh
+++ infra/dev/minikube/scripts/configure-docker.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -e
 
 sudo adduser docker -g docker
+echo "docker ALL=(ALL) NOPASSWD:ALL" >> docker-user
+sudo chmod 440 docker-user
+sudo chown root:root docker-user
+sudo mv docker-user /etc/sudoers.d/docker-user
--- infra/dev/minikube/scripts/forward-port-to-minikube.sh
+++ infra/dev/minikube/scripts/forward-port-to-minikube.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
 
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 8443 -j DNAT --to-destination 192.168.49.2
+sudo su - docker -c "nohup minikube tunnel &"
