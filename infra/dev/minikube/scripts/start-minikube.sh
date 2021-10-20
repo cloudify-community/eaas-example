ed522177f47817453de68ceeee15b3fd1dbbdc52
jrzeszutek <jaroslaw@cloudify.co>
2021-10-20 12:40:12 +0200

    fixed minikube

--- infra/dev/minikube/scripts/start-minikube.sh
+++ infra/dev/minikube/scripts/start-minikube.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 sudo su - docker -c "minikube start --apiserver-ips ${HOST_VM_IP}"
-sudo su - docker -c "nohup minikube tunnel &"
+sudo su - docker -c "nohup minikube tunnel &> /dev/null &"
